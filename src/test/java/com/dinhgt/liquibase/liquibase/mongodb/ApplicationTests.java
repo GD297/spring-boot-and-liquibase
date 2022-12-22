a1966265feb2cc802d9cea15e107493bf2bbf562
GDante <dinhgiang443@gmail.com>
2022-12-22 22:47:45 +0700

    DINHGT: Init project

---
+++ src/test/java/com/dinhgt/liquibase/liquibase/mongodb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinhgt.liquibase.liquibase.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
